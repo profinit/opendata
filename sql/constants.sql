634e47dd0df956901f7fd6c09a8d4b0c149494bf
Daniel Malý <malydani@fit.cvut.cz>
2015-11-08 19:01:53 +0100

    Pridan skript pro naplneni enumerovanych tabulek

---
+++ sql/constants.sql
@@ -0,0 +1,18 @@
+/* Fills tables representing enumerated types */
+
+INSERT INTO authority_role (authority_role) VALUES ('supplier');
+INSERT INTO authority_role (authority_role) VALUES ('customer');
+
+INSERT INTO record_type (record_type) VALUES ('contract');
+INSERT INTO record_type (record_type) VALUES ('invoice');
+INSERT INTO record_type (record_type) VALUES ('order');
+
+INSERT INTO periodicity (periodicity) VALUES ('yearly');
+INSERT INTO periodicity (periodicity) VALUES ('monthly');
+INSERT INTO periodicity (periodicity) VALUES ('quarterly');
+INSERT INTO periodicity (periodicity) VALUES ('aperiodic');
+
+INSERT INTO entity_type (entity_type) VALUES ('ministry');
+INSERT INTO entity_type (entity_type) VALUES ('company');
+INSERT INTO entity_type (entity_type) VALUES ('individual');
+INSERT INTO entity_type (entity_type) VALUES ('other');
