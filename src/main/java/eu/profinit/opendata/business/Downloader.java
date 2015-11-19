83a5f5390feaae46462a7e89d7ab0923d9449982
Daniel Malý <malydani@fit.cvut.cz>
2015-11-19 10:29:17 +0100

    Downloader

---
+++ src/main/java/eu/profinit/opendata/business/Downloader.java
@@ -0,0 +1,25 @@
+package eu.profinit.opendata.business;
+
+import eu.profinit.opendata.model.DataInstance;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+
+/**
+ * Created by DM on 19. 11. 2015.
+ */
+public class Downloader {
+
+    public InputStream downloadIntoOutputStream(DataInstance dataInstance) throws MalformedURLException, IOException {
+        URL url  = new URL(dataInstance.getUrl());
+        ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+        return Channels.newInputStream(rbc);
+    }
+
+
+}
---
+++ src/test/java/eu/profinit/opendata/test/DownloaderTest.java
@@ -0,0 +1,7 @@
+package eu.profinit.opendata.test;
+
+import static org.junit.Assert.*;
+
+public class DownloaderTest {
+
+}
