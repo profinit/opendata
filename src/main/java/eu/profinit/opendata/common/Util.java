ed4296ca0fe3b3b3baa02e77b6bc5d73a3baddd3
Daniel Maly <maly.dan@gmail.com>
2016-01-31 15:13:17 +0100

    Opomenutý soubor

---
+++ src/main/java/eu/profinit/opendata/common/Util.java
@@ -0,0 +1,10 @@
+package eu.profinit.opendata.common;
+
+/**
+ * Created by dm on 1/31/16.
+ */
+public class Util {
+    public static boolean isNullOrEmpty(String s) {
+        return s == null || s.isEmpty();
+    }
+}
