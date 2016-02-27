ad049b45fc04a709b535ae6ca77eef073dcb8c76
Daniel Maly <maly.dan@gmail.com>
2016-02-27 12:00:53 +0100

    Přidány smlouvy Ministerstva pro místní rozvoj

--- sql/data.sql
+++ sql/data.sql
@@ -106,5 +106,48 @@ INSERT INTO data_instance(data_source_id, url, format, periodicity, description,
 
   (
     (SELECT  data_source_id FROM invoices_ds), 'http://www.mkcr.cz/assets/povinne-zverejnovane-informace/Uhrazene-faktury-dobropisy-a-platebni-poukazy_leden_2015.xlsx',
-    'xlsx', 'aperiodic', 'Faktury MK leden 2015', 'mappings/mk/mapping-invoices.xml', TRUE
+    'xlsx', 'aperiodic', 'Faktury MK leden 2015', 'mappings/mk/mapping-invoices.xml', FALSE
+  );
+
+-- MMR: Data instances are manual and experimentally periodic, but we don't know how updates are published.
+
+WITH mmr AS (INSERT INTO entity(entity_type, name, ico, is_public) VALUES
+  ('ministry', 'Ministerstvo pro místní rozvoj', '66002222', TRUE) RETURNING entity_id),
+
+    contracts_ds AS (
+    INSERT INTO data_source (entity_id, record_type, periodicity, handling_class, active, description) VALUES (
+      (SELECT entity_id FROM mmr),
+      'contract', 'aperiodic', 'eu.profinit.opendata.control.BlankHandler', TRUE, 'Smlouvy MMR')
+    RETURNING data_source_id
+  ),
+
+    invoices_ds AS (
+    INSERT INTO data_source (entity_id, record_type, periodicity, handling_class, active, description) VALUES (
+      (SELECT entity_id FROM mmr),
+      'invoice', 'aperiodic', 'eu.profinit.opendata.control.BlankHandler', TRUE, 'Faktury MMR')
+    RETURNING data_source_id
+  ),
+
+    orders_ds AS (
+    INSERT INTO data_source (entity_id, record_type, periodicity, handling_class, active, description) VALUES (
+      (SELECT entity_id FROM mmr),
+      'order', 'aperiodic', 'eu.profinit.opendata.control.BlankHandler', TRUE, 'Objednávky MMR')
+    RETURNING data_source_id
+  )
+
+INSERT INTO data_instance(data_source_id, url, format, periodicity, description, mapping_file, incremental) VALUES
+
+  (
+    (SELECT  data_source_id FROM contracts_ds), 'http://www.mmr.cz/getmedia/3418880e-894f-4cff-9e9e-62a172394c85/Smlouvy_1.xlsx',
+    'xlsx', 'monthly', 'Smlouvy MMR od 1. 1. 2015', 'mappings/mmr/mapping-contracts.xml', FALSE
+  ),
+
+  (
+    (SELECT  data_source_id FROM invoices_ds), 'http://www.mmr.cz/getmedia/30c36454-8062-4649-9b55-fbb89d1c86e5/Faktury-2015_2.xlsx',
+    'xlsx', 'monthly', 'Faktury MMR od 1. 1. 2015', 'mappings/mmr/mapping-invoices.xml', FALSE
+  ),
+
+  (
+    (SELECT  data_source_id FROM orders_ds), 'http://www.mmr.cz/getmedia/2a2ea9c8-e6d5-46f5-8d7b-f961b8fabbce/Objednavky_2.xlsx',
+    'xlsx', 'monthly', 'Objednávky MMR od 1. 1. 2015', 'mappings/mmr/mapping-orders.xml', FALSE
   );
--- sql/differentials/230216.sql
+++ sql/differentials/230216.sql
@@ -8,14 +8,14 @@ WITH mk AS (INSERT INTO entity(entity_type, name, ico, dic, is_public) VALUES
     contracts_ds AS (
     INSERT INTO data_source (entity_id, record_type, periodicity, handling_class, active, description) VALUES (
       (SELECT entity_id FROM mk),
-      'contract', 'aperiodic', 'eu.profinit.opendata.control.GenericDataSourceHandler', TRUE, 'Smlouvy MK')
+      'contract', 'aperiodic', 'eu.profinit.opendata.control.BlankHandler', TRUE, 'Smlouvy MK')
     RETURNING data_source_id
   ),
 
     invoices_ds AS (
     INSERT INTO data_source (entity_id, record_type, periodicity, handling_class, active, description) VALUES (
       (SELECT entity_id FROM mk),
-      'invoice', 'aperiodic', 'eu.profinit.opendata.control.GenericDataSourceHandler', TRUE, 'Faktury MK')
+      'invoice', 'aperiodic', 'eu.profinit.opendata.control.BlankHandler', TRUE, 'Faktury MK')
     RETURNING data_source_id
   )
 
@@ -28,5 +28,5 @@ INSERT INTO data_instance(data_source_id, url, format, periodicity, description,
 
   (
     (SELECT  data_source_id FROM invoices_ds), 'http://www.mkcr.cz/assets/povinne-zverejnovane-informace/Uhrazene-faktury-dobropisy-a-platebni-poukazy_leden_2015.xlsx',
-    'xlsx', 'aperiodic', 'Faktury MK leden 2015', 'mappings/mk/mapping-invoices.xml', TRUE
+    'xlsx', 'aperiodic', 'Faktury MK leden 2015', 'mappings/mk/mapping-invoices.xml', FALSE
   );
